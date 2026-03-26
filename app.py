6251fbbe4aedf4e2c5c4813db5aaf56fe9fcd5d6
karthik-j7799 <kart33539@gmail.com>
2026-03-27 02:01:57 +0530

    add render config

--- app.py
+++ app.py
@@ -88,4 +88,6 @@ def fill_template(tex, data):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
